48d100a5a26a22232d8dccbaeaecfc42d1453d68
Andres Alzate <aalzate86@gmail.com>
2016-08-11 21:43:20 -0500

    first commit with app test

---
+++ src/main/java/co/andres/alzate/app/main/Main.java
@@ -0,0 +1,26 @@
+package co.andres.alzate.app.main;
+
+import org.apache.spark.api.java.*;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
+import static spark.Spark.*;
+
+
+/**
+ * Created by afprog on 8/08/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello Heroku World");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+}

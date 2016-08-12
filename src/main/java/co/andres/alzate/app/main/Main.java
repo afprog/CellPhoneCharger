a2c0c432507a33f1963f50ee11db2cb0ce227275
Andres Alzate <aalzate86@gmail.com>
2016-08-12 00:23:24 -0500

    changes in path and log

--- src/main/java/co/andres/alzate/app/main/Main.java
+++ src/main/java/co/andres/alzate/app/main/Main.java
@@ -4,6 +4,13 @@ import org.apache.log4j.Logger;
 import org.apache.spark.api.java.*;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
+import spark.Filter;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
+import java.util.HashMap;
+
 import static spark.Spark.*;
 
 
@@ -17,7 +24,50 @@ public class Main {
         int port = getHerokuAssignedPort();
         logger.info("Port found" + port);
         port(port);
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Request-Method", "GET,PUT,POST,DELETE,OPTIONS");
+            response.header("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
+            // Note: this may or may not be necessary in your particular application
+            response.type("application/json");
+        });
         get("/hello", (req, res) -> "Hello Heroku World");
+    }
+
+    private static final HashMap<String, String> corsHeaders = new HashMap<String, String>();
+
+    static {
+        corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        corsHeaders.put("Access-Control-Allow-Origin", "*");
+        corsHeaders.put("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
+        corsHeaders.put("Access-Control-Allow-Credentials", "true");
+    }
+
+    public final static void apply() {
+        Filter filter = new Filter() {
+            @Override
+            public void handle(Request request, Response response) throws Exception {
+                corsHeaders.forEach((key, value) -> {
+                    response.header(key, value);
+                });
+            }
+        };
+        Spark.after(filter);
     }
 
     static int getHerokuAssignedPort() {
